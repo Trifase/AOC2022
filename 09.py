11e156d5470a3bb0731c8ec191e035cbe9de7818
Luca Bellanti <luca.bellanti@gmail.com>
2022-12-09 14:41:03 +0100

    Added comments to Day 9, cleanup

--- 09.py
+++ 09.py
@@ -12,6 +12,10 @@ DAY = 9
 
 @dataclass
 class MovingThing:
+    """
+    This is the base class of a moving things in a 2D matrix. It has two coords (x and y) and
+    a move() function that takes a direction ((L)eft, (R)ight, (U)p and (D)own) and optionally an amount.
+    """
     x: int = 0
     y: int = 0
     coords : tuple[int, int] = (0, 0)
@@ -27,13 +31,24 @@ class MovingThing:
                 self.x -= units
 
         self.coords = (self.x, self.y)
-        # print(f"head: mi sposto su {self.coords}")
 
 
 class RopePiece(MovingThing):
+    """
+    This is a piece of a rope, based on Movingthing.
+    It can be attached to another MovingThing with the attach_to parameter.
+    The function follow() is used to move this piece, it takes no parameters.
+    """
     attached_to: MovingThing = None
 
     def follow(self, head: MovingThing= None):
+        """
+        This is used to move the piece based on the position of the piece it is attached_to.
+        If there is not such piece, raise AttributeError.
+        First, it calculates the Chess Distance (https://en.wikipedia.org/wiki/Chebyshev_distance)
+        from the piece it is attached to, and if it's more than 1, it moves accordingly in one 
+        of the 4 cardinal directions or in diagonal.  
+        """
         def sign(a: int):
             if a > 0:
                 return 1
@@ -41,43 +56,40 @@ class RopePiece(MovingThing):
                 return -1
             else:
                 return 0
+
         if not self.attached_to:
             raise AttributeError("Non sono attaccato a niente")
 
         head = self.attached_to
         dx, dy = (head.x - self.x), (head.y - self.y)
 
-        if abs(dx) <= 1 and abs(dy) <= 1:
+        if abs(dx) <= 1 and abs(dy) <= 1: # Chess Distance
             pass
-            # print("tail: Sto fermo")
         else:
             x = sign(dx)
             y = sign(dy)
             self.x += x
             self.y += y
-            # print(f"tail: mi sposto su ({self.x}, {self.y})")
             self.coords = (self.x, self.y)
 
-
-#Input parsing
+# Input parsing
 with Timer(name="Parsing", text="Parsing done: \t{milliseconds:.0f} ms"):
     """
     We'll parse the input line by line. 
     """
     data = get_data(YEAR, DAY, SESSIONS, strip=True, example=False)
 
-
-
-
-
-
-
 # Part 1
 @Timer(name="Part 1", text="Part 1 done: \t{milliseconds:.0f} ms")
 def part1(data: list[str]):
-
     """
-
+    We'll instance a Head and a Tail of a rope.
+    Tail will be attached_to Head.
+    We'll parse every line to grab the (dir)ection
+    and the (amount) of moving steps.
+    For every step in (dir), we'll move head with head.move() and we'll move the tail with tail.move().
+    We'll save every position tail visits in a (visited) set (so we won't have duplicates).
+    We'll solve getting the lenght of the set.
     """
 
     sol1 = 0
@@ -86,14 +98,14 @@ def part1(data: list[str]):
     visited.add((0, 0))
 
     head = RopePiece()
-    tail1 = RopePiece(attached_to=head)
+    tail = RopePiece(attached_to=head)
 
     for line in data:
         dir, amount = line.split()
         for _ in range(int(amount)):
             head.move(dir)
-            tail1.follow()
-            visited.add(tail1.coords)
+            tail.follow()
+            visited.add(tail.coords)
 
     sol1 = len(visited)
 
@@ -104,7 +116,14 @@ def part1(data: list[str]):
 @Timer(name="Part 2", text="Part 2 done: \t{milliseconds:.0f} ms")
 def part2(data):
     """
-
+    In a list (rope) we'll add a head.
+    Then we will append 9 tail pieces, every one of them will be attached_to to the previous.
+    We'll parte every line to grab the (dir)ection
+    and the (amount) of moving steps.
+    For every step in (dir), we'll move head with head.move() and we'll move all the tail pieces
+    with a for loop. 
+    We'll save every position the last tail visits in a (visited) set (so we won't have duplicates).
+    We'll solve getting the lenght of the set.
     """
 
     sol2 = 0
